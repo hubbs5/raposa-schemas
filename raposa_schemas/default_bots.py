c82dd41b9c63e2d3772e6637e1321eeaedb5d9c7
Connor Valentine <cvalentine91@gmail.com>
2022-06-03 15:46:25 -0400

    Update default_bots.py

--- raposa_schemas/default_bots.py
+++ raposa_schemas/default_bots.py
@@ -1,22 +1,24 @@
-'''
+"""
 This is a very rudimentary way to create the default bots for
 users without accounts. (or who have not saved their bots).
 
 Currently hard-coded. But we could maek it more dynamic.
-'''
+"""
+
 
 def get_default_bot(bot_number):
     preset1 = {
         "strategy_settings": {
             "account_size": 10000.0,
+            "init_date": "2014-11-12",
             "start_date": "2015-01-01",
             "end_date": "2017-12-31",
-            "instruments": ["ABT"],
-            "trade_days": ["mon", "tue","wed","thu","fri"],
+            "instruments": ["TSLA"],
+            "trade_days": ["mon", "tue", "wed", "thu", "fri"],
             "trade_frequency": 1,
             "position_sizing_strategy": {"name": "EqualAllocation", "params": {}},
             "position_management_strategy": {"name": "EqualAllocation", "params": {}},
-            "rebalance_days": ["mon", "tue","wed","thu","fri"],
+            "rebalance_days": ["mon", "tue", "wed", "thu", "fri"],
             "rebalance_frequency": 1,
         },
         "buy_signals": {
@@ -24,17 +26,17 @@ def get_default_bot(bot_number):
                 {
                     "indicator": {
                         "name": "SMA",
-                        "params": {"period": 20},
+                        "params": {"period": 30},
                         "needs_comp": True,
                         "valid_comps": ["SMA", "EMA", "MACD", "PRICE"],
                     },
                     "comp_indicator": {
-                        "name": "EMA",
-                        "params": {"period": 50},
+                        "name": "PRICE",
+                        "params": {"price_type": "High"},
                         "needs_comp": True,
-                        "valid_comps": ["SMA", "EMA", "MACD", "PRICE"],
+                        "valid_comps": ["SMA", "EMA", "MACD", "ATR"],
                     },
-                    "rel": "geq",
+                    "rel": "leq",
                     "short": False,
                 }
             ]
@@ -44,17 +46,17 @@ def get_default_bot(bot_number):
                 {
                     "indicator": {
                         "name": "STOP_PRICE",
-                        "params": {"percent_change": -10},
+                        "params": {"percent_change": 3, "trailing": False},
                         "needs_comp": True,
                         "valid_comps": ["PRICE"],
                     },
                     "comp_indicator": {
                         "name": "PRICE",
-                        "params": {'price_type': 'High'},
+                        "params": {"price_type": "Close"},
                         "needs_comp": True,
                         "valid_comps": ["SMA", "EMA", "MACD", "ATR"],
                     },
-                    "rel": "leq",
+                    "rel": "eq",
                     "short": False,
                 }
             ]
@@ -65,19 +67,22 @@ def get_default_bot(bot_number):
     preset2 = {
         "strategy_settings": {
             "account_size": 10000.0,
+            "init_date": "2017-10-11",
             "start_date": "2018-01-01",
             "end_date": "2019-12-31",
             "instruments": ["GE"],
-            "trade_days": ["mon","tue","wed","thu","fri"],
+            "trade_days": ["mon", "tue", "wed", "thu", "fri"],
             "trade_frequency": 1,
             "position_sizing_strategy": {"name": "EqualAllocation", "params": {}},
-            "position_management_strategy": {"name": "VOLATILITYSizing",
-                                             "params": {
-                                                 "period": 356,
-                                                 "risk_coefficient": 1,
-                                                 "max_position_risk_frac": 0.04,
-                                             }},
-            "rebalance_days": ["mon","tue","wed","thu","fri"],
+            "position_management_strategy": {
+                "name": "VOLATILITYSizing",
+                "params": {
+                    "period": 356,
+                    "risk_coefficient": 1,
+                    "max_position_risk_frac": 0.04,
+                },
+            },
+            "rebalance_days": ["mon", "tue", "wed", "thu", "fri"],
             "rebalance_frequency": 1,
         },
         "buy_signals": {
@@ -129,16 +134,18 @@ def get_default_bot(bot_number):
             "start_date": "2005-01-01",
             "end_date": "2007-12-31",
             "instruments": ["ATVI"],
-            "trade_days": ["mon","tue","wed","thu","fri"],
+            "trade_days": ["mon", "tue", "wed", "thu", "fri"],
             "trade_frequency": 1,
             "position_sizing_strategy": {"name": "EqualAllocation", "params": {}},
-            "position_management_strategy": {"name": "VOLATILITYSizing",
-                                             "params": {
-                                                 "period": 90,
-                                                 "risk_coefficient": 0.7,
-                                                 "max_position_risk_frac": 0.05,
-                                             }},
-            "rebalance_days": ["mon","tue","wed","thu","fri"],
+            "position_management_strategy": {
+                "name": "VOLATILITYSizing",
+                "params": {
+                    "period": 90,
+                    "risk_coefficient": 0.7,
+                    "max_position_risk_frac": 0.05,
+                },
+            },
+            "rebalance_days": ["mon", "tue", "wed", "thu", "fri"],
             "rebalance_frequency": 1,
         },
         "buy_signals": {
@@ -193,13 +200,15 @@ def get_default_bot(bot_number):
             "trade_days": ["mon", "tue", "wed", "thu", "fri"],
             "trade_frequency": 1,
             "position_sizing_strategy": {"name": "EqualAllocation", "params": {}},
-            "position_management_strategy": {"name": "VOLATILITYSizing",
-                                             "params": {
-                                                 "period": 30,
-                                                 "risk_coefficient": 1,
-                                                 "max_position_risk_frac": 0.02,
-                                             }},
-            "rebalance_days": ["mon","tue","wed","thu","fri"],
+            "position_management_strategy": {
+                "name": "VOLATILITYSizing",
+                "params": {
+                    "period": 30,
+                    "risk_coefficient": 1,
+                    "max_position_risk_frac": 0.02,
+                },
+            },
+            "rebalance_days": ["mon", "tue", "wed", "thu", "fri"],
             "rebalance_frequency": 1,
         },
         "buy_signals": {
