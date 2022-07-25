1eec59ed1fb4d422bb58853d7cdeb1ec7c6149cb
Connor Valentine <cvalentine91@gmail.com>
2022-07-25 10:19:07 -0400

    Update schemas.py
    added documentaton

--- raposa_schemas/schemas.py
+++ raposa_schemas/schemas.py
@@ -19,38 +19,22 @@
 """
 
 
-"""
-notes about pydantic and Typing
-    - Union from typing allows for entries to be of either type BUT the 
-    first listed will be prioritized. This WILL convert entry types.
 
-    Pydantic Classes that are generated with Basemodel have a few ways to see the data
-    ex: 
-        indicator = SMA(signal_name = 'SMA', period = 2)
-        print(SMA.json())
-        print(SMA.dict())
-        print(SMA.schema_json(indent=2)) # outputs muct more information about the model including default values and field type
-
-    Pydantic fields:
-        - if you do not list a default value the field is mandatory
-        - you must specify either the type of each field, 
-            not really, but there are conseuquences of mixing specification and not in one model see docs)
-
-    - You can make an instance of a class with this code:
-        indicator_klass = globals()['PRICE'] 
-        params = indicator_klass.__fields__['params']
-        # This will make an object of the class that is hard to work with, but can be populated with
-        temp_klass = indicator_klass()
-        print(temp_klass.params
 
 """
+How to add a new indicator to the website
+    1) create a schema class here
+    2) Before issuing the PR to raposa-schemas, 
+        pip install the branch onto your local raposa-website using pip install -e git+https://git@github.com/hubbs5/raposa-schemas.git@{ branch name }#egg=raposa-schemas
+        pip install the branch onto your local raposa using pip install -e git+https://git@github.com/hubbs5/raposa-schemas.git@{ branch name }#egg=raposa-schemas
+    3) Add the indicator to raposa-website\apps\dash_strategy_builder\dash_app_utils\indicator_divs.py
+        It is not best practice, but you may need to add a special case to 
+        raposa-website\apps\dash_strategy_builder\dash_app_utils\strategy_compilation.py for indicators that do not follow the standard formula
+    4) test to make sure backtests can run with the new indicator on the frontend
+    5) update the bot garage so that it can process the indicator in new strategies
+        update signal_translator() in apps\dash_bot_garage\dash_app_utils\utils.py
 
-"""
-How to add a new strategy option to the site that requires parameters (position sizing, risk management, etc)
-    - create the new dropdowns in buy_tab, sell_tab, or strategy_tab
-    - create the callbacks buy_tab, sell_tab, or strategy_tab
-    - add the new inputs to dash_app_main.build_strategy callback
-    - update utils_strategy_compilation.py
+    -
     
 """
 from typing import List, Type, Union, Optional
@@ -183,6 +167,31 @@ indicators_with_time_params = {
 
 relations = {"> or =": "geq", "< or =": "leq", ">": "gt", "<": "lt", "=": "eq"}
 
+"""
+notes about pydantic and Typing
+    - Union from typing allows for entries to be of either type BUT the 
+    first listed will be prioritized. This WILL convert entry types.
+
+    Pydantic Classes that are generated with Basemodel have a few ways to see the data
+    ex: 
+        indicator = SMA(signal_name = 'SMA', period = 2)
+        print(SMA.json())
+        print(SMA.dict())
+        print(SMA.schema_json(indent=2)) # outputs muct more information about the model including default values and field type
+
+    Pydantic fields:
+        - if you do not list a default value the field is mandatory
+        - you must specify either the type of each field, 
+            not really, but there are conseuquences of mixing specification and not in one model see docs)
+
+    - You can make an instance of a class with this code:
+        indicator_klass = globals()['PRICE'] 
+        params = indicator_klass.__fields__['params']
+        # This will make an object of the class that is hard to work with, but can be populated with
+        temp_klass = indicator_klass()
+        print(temp_klass.params
+
+"""
 
 """ all of these indicator classes have the same number of inputs
 as required to run in pyalgotrade"""
